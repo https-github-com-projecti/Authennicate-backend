a58dfdac3d30c180a9c328814286b5051d2f78b0
narawichsaphimarn <NarawichSaphimarn@gmail.com>
2020-10-05 09:28:17 +0700

    fix port

--- authenName/properties/properties.go
+++ authenName/properties/properties.go
@@ -1,7 +1,8 @@
 package properties
 
 const (
-	PortServe = ":3001"
+	PortServe = ":8080"
 	MongoServer = "mongodb://admin:password@checkname.accesscam.org:27017"
 	Path = "\\\\Win-04bs1plpe5t\\d"
+	//Path = "D:"
 )
