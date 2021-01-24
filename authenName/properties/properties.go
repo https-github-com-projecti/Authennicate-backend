ea1bd7f271655637f95720a97d06b1d7af975978
Narawich Saphimarn (Ball)\Naras <naras529@vanpl.postbox.in.th>
2021-01-24 11:26:29 +0700

    update docker

--- authenName/properties/properties.go
+++ authenName/properties/properties.go
@@ -2,7 +2,7 @@ package properties
 
 const (
 	PortServe = ":8080"
-	MongoServer = "mongodb://admin:password@checkname.accesscam.org:27017"
+	MongoServer = "mongodb://admin:password@fourdust.kozow.com:27017"
 	Path = "\\\\Win-04bs1plpe5t\\d"
 	//Path = "D:"
 )
